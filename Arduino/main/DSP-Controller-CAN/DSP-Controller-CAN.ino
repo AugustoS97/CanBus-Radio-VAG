c9286d4d82ebfeeef21bb029bda37ec591eeeed2
Augusto S Hernandez <augustosamuelhdezmartin@gmail.com>
2023-01-17 23:39:58 +0000

    Some changes in code

--- Arduino/main/DSP-Controller-CAN/DSP-Controller-CAN.ino
+++ Arduino/main/DSP-Controller-CAN/DSP-Controller-CAN.ino
@@ -1,41 +1,48 @@
-#include <mcp_can.h>
+/* Arduino code to send CAN message to enable some car functions in Volkswagen, Skoda, SEAT and Audi (VW AG).
+ * This version allows to enable the original sound amplifier with a non-original radio.
+ * Tested in a Skoda Octavia 1Z (2005) Edition 100 with Sound System (12 speakers)
+ * Author: Augusto Samuel Hernandez Martin (Github: @AugustoS97)
+ * Date: 10/01/2023
+ */
 
+//Libraries to work with can bus
+#include <mcp_can.h> 
 #include <SPI.h>
 
 MCP_CAN CAN0(10);     // Set CS to pin 10
 
-void setup()
+void setup(){
 
-{
+  Serial.begin(115200); //Serial COM to debug
 
-  Serial.begin(115200);
+  // Initialize MCP2515 running at 8MHz with a baudrate of 100kb/s and the masks and filters disabled.
+  // Infotainment CAN BUS uses 100kb/s in Skoda.
 
-  // Initialize MCP2515 running at 16MHz with a baudrate of 500kb/s and the masks and filters disabled.
+  if(CAN0.begin(MCP_ANY, CAN_100KBPS, MCP_8MHZ) == CAN_OK){
+    Serial.println("MCP2515 Initialized Successfully!");
+  }
 
-  if(CAN0.begin(MCP_ANY, CAN_100KBPS, MCP_8MHZ) == CAN_OK) Serial.println("MCP2515 Initialized Successfully!");
-
-  else Serial.println("Error Initializing MCP2515...");
+  else{ //If there is an error initializing  interface, wait for a reset here.
+    Serial.println("Error Initializing MCP2515...");
+    while(true){
+      delay(10);
+    }
+  }
 
   CAN0.setMode(MCP_NORMAL);   // Change to normal mode to allow messages to be transmitted
 
 }
 
-void loop()
+void loop(){
 
-{
-
-  // send data:  ID = 0x100, Standard CAN Frame, Data length = 8 bytes, 'data' = array of data bytes to send
+  // Send data to enable Spekaer Amplifiers: 
+  // ID = 0x507, Standard CAN Frame, Data length = 6 bytes, 'data' = array of data bytes to send
+  // Data: 0x03, 0x00, 0x00, 0x00, 0x00, 0x00
 
   unsigned char stmp0[6] = {0x03, 0x00, 0x00, 0x00, 0x00, 0x00};
 
   byte sndStat0 = CAN0.sendMsgBuf(0x507, 0, 6, stmp0);
 
-  delay(5000);   // send data per 5000ms
+  delay(5000);   // send data every 5000ms
 
 }
-
-/*********************************************************************************************************
-
-  END FILE
-
-*********************************************************************************************************/
---
+++ Arduino/tools/Can-Radio-tester/Can-Radio-tester.ino
@@ -0,0 +1,38 @@
+/* Tool to read the CAN BUS and check if the Radio Interface is working well.
+ * Return by serial all the can messages received.
+ * Author: Augusto Samuel Hernandez Martin (Github: @AugustoS97)
+ */
+
+#include <SPI.h>
+#include <mcp2515.h>
+
+struct can_frame canMsg;
+MCP2515 mcp2515(10);
+
+
+void setup() {
+  Serial.begin(115200);
+  
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_100KBPS, MCP_8MHZ);
+  mcp2515.setNormalMode();
+  
+  Serial.println("------- CAN Read ----------");
+  Serial.println("ID  DLC   DATA");
+}
+
+void loop() {
+  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+    Serial.print(canMsg.can_id, HEX); // print ID
+    Serial.print(" "); 
+    Serial.print(canMsg.can_dlc, HEX); // print DLC
+    Serial.print(" ");
+    
+    for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
+      Serial.print(canMsg.data[i],HEX);
+      Serial.print(" ");
+    }
+
+    Serial.println();      
+  }
+}
